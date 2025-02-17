a3474eab3daebe6e8a06201002fe28631dad8682
flixplayer <dhaliwal.saav@gmail.com>
2025-02-17 19:18:32 +0100

    Update process.php

--- process.php
+++ process.php
@@ -21,7 +21,32 @@ include 'functions.php';
 
 if (isset($_GET['profile'])) {
     header('Content-Type: application/json');
-    echo getProfile();
+    $profile =  json_decode(getProfile(), true);
+    
+    $fname =  $profile['js']['fname'];
+    echo " 
+    Name:$fname";
+    
+    $mac =  $profile['js']['mac'];
+    echo "  
+    
+    MAC:$mac";
+    
+    $parent_password =  $profile['js']['parent_password'];
+    echo "  
+    
+    Adult:$parent_password";
+    
+    $expire_billing_date =  $profile['js']['expire_billing_date'];
+    echo "  
+    
+    Expire Date:$expire_billing_date";
+    
+    $stb_type =  $profile['js']['stb_type'];
+    echo "  
+    
+    STB:$stb_type";
+    
     exit();
 }
 
